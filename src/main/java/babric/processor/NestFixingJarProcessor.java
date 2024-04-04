1b6087659708a47faf4032ff217db1ed80106960
Cat Core <arthur.koalas@free.fr>
2024-04-05 00:42:29 +0200

    Fix NestFixer Spec hashcode

--- src/main/java/babric/processor/NestFixingJarProcessor.java
+++ src/main/java/babric/processor/NestFixingJarProcessor.java
@@ -3,28 +3,25 @@ package babric.processor;
 import net.fabricmc.loom.api.processor.MinecraftJarProcessor;
 import net.fabricmc.loom.api.processor.ProcessorContext;
 import net.fabricmc.loom.api.processor.SpecContext;
+import net.fabricmc.loom.util.fmj.FabricModJson;
 import net.fabricmc.stitch.commands.CommandFixNesting;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 public class NestFixingJarProcessor implements MinecraftJarProcessor<NestFixingJarProcessor.Spec> {
 
     @Override
     public @Nullable NestFixingJarProcessor.Spec buildSpec(SpecContext specContext) {
-        return new Spec();
+        return new Spec(specContext.allMods());
     }
 
     @Override
     public void processJar(Path path, Spec spec, ProcessorContext processorContext) throws IOException {
-        File file = path.getParent().resolve(".fixedNest").toFile();
-
-        if (!file.exists()) {
-            CommandFixNesting.run(path.toFile());
-            file.createNewFile();
-        }
+        CommandFixNesting.run(path.toFile());
     }
 
     @Override
@@ -32,5 +29,5 @@ public class NestFixingJarProcessor implements MinecraftJarProcessor<NestFixingJ
         return "babric:fix-nesting";
     }
 
-    public static class Spec implements MinecraftJarProcessor.Spec {}
+    public record Spec(List<FabricModJson> modJsons) implements MinecraftJarProcessor.Spec {}
 }
