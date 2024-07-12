a721e0a1b2c74e9561750c5d0a21af6efb352835
calmilamsy <bumbill00@gmail.com>
2024-07-12 08:48:05 +0100

    Update Constants.java

--- src/main/java/babric/Constants.java
+++ src/main/java/babric/Constants.java
@@ -3,5 +3,5 @@ package babric;
 public class Constants {
     public static final String LEGACYFABRIC_MAVEN = "https://repo.legacyfabric.net/repository/legacyfabric/";
     public static final String BABRIC_MAVEN = "https://maven.glass-launcher.net/babric/";
-    public static final String MODRINTH_MAVEN = "https://api.modrinth.com/maven/";
+    public static final String GLASS_MAVEN = "https://maven.glass-launcher.net/releases/";
 }
