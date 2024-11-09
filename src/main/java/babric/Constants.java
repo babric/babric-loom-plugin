1503a9798cd3736cd787e0bdced04e9b89505295
calmilamsy <bumbill00@gmail.com>
2024-11-09 17:13:23 +0000

    Fix not being able to do anything

--- src/main/java/babric/Constants.java
+++ src/main/java/babric/Constants.java
@@ -1,7 +1,7 @@
 package babric;
 
 public class Constants {
-    public static final String LEGACYFABRIC_MAVEN = "https://repo.legacyfabric.net/repository/legacyfabric/";
+    public static final String LEGACYFABRIC_MAVEN = "https://repo.legacyfabric.net/legacyfabric/";
     public static final String BABRIC_MAVEN = "https://maven.glass-launcher.net/babric/";
     public static final String GLASS_MAVEN = "https://maven.glass-launcher.net/releases/";
 }
