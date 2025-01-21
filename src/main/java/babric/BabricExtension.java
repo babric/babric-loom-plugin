08351fa56f62384d234f463fd35d86a57f9e3884
calmilamsy <bumbill00@gmail.com>
2025-01-21 06:52:24 +0000

    Subproject Helper Methods, Add an Idiot-Proof Pom Stuffing Fix (#10)

--- src/main/java/babric/BabricExtension.java
+++ src/main/java/babric/BabricExtension.java
@@ -6,7 +6,10 @@ import org.gradle.api.provider.Property;
 public class BabricExtension {
     public final Property<Boolean> disableM1Fixes;
 
+    public final Property<Boolean> disablePomOverride;
+
     public BabricExtension(Project project) {
         disableM1Fixes = project.getObjects().property(Boolean.class).convention(false);
+        disablePomOverride = project.getObjects().property(Boolean.class).convention(false);
     }
 }
--- src/main/java/babric/BabricLoomPlugin.java
+++ src/main/java/babric/BabricLoomPlugin.java
@@ -4,13 +4,18 @@ import babric.mappings.BabricIntermediaryProvider;
 import babric.processor.GambacLibraryProcessor;
 import babric.processor.LWJGL2LibraryProcessor;
 import babric.processor.NestFixingJarProcessor;
+import groovy.util.Node;
+import groovy.util.NodeList;
 import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.api.LoomGradleExtensionAPI;
 import net.fabricmc.loom.configuration.providers.minecraft.library.LibraryProcessorManager;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.plugins.PluginAware;
 import org.gradle.api.provider.ListProperty;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.internal.os.OperatingSystem;
 
 import java.util.Map;
@@ -27,15 +32,20 @@ public class BabricLoomPlugin implements Plugin<PluginAware> {
         if (target instanceof Project project) applyProject(project);
     }
 
+    @SuppressWarnings("UnstableApiUsage")
     private void applyProject(Project project) {
         project.getLogger().lifecycle("Babric loom: " + VERSION);
+
+        BabricExtension babricExtension = project.getExtensions().create("babric", BabricExtension.class, project);
+
+        // Add a transitiveImplementation configuration because gradle has no built-in way to say "don't add this to the pom"
+        Configuration transitiveImplementation = project.getConfigurations().create("transitiveImplementation");
+        project.getConfigurations().getByName("implementation").extendsFrom(transitiveImplementation);
 
         ListProperty<LibraryProcessorManager.LibraryProcessorFactory> libraryProcessors = LoomGradleExtension.get(project).getLibraryProcessors();
         libraryProcessors.add(LWJGL2LibraryProcessor::new);
 
         LoomGradleExtensionAPI extension = (LoomGradleExtensionAPI) project.getExtensions().getByName("loom");
-
-        BabricExtension babricExtension = project.getExtensions().create("babric", BabricExtension.class, project);
 
         libraryProcessors.add((platform, libraryContext) -> new GambacLibraryProcessor(platform, libraryContext, extension, project, babricExtension));
 
@@ -53,6 +63,32 @@ public class BabricLoomPlugin implements Plugin<PluginAware> {
                 extension.getRunConfigs().configureEach(runConfig -> {
                     if (runConfig.getName().equals("client")) {
                         runConfig.getVmArgs().add("-Dapple.awt.application.appearance=system");
+                    }
+                });
+            }
+        });
+
+        // Wipes the normal pom's dependency block and substitutes in the transitiveImplementation configuration contents.
+        PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+        project.afterEvaluate(p -> {
+            if (!babricExtension.disablePomOverride.get()) {
+                publishing.getPublications().forEach(publication -> {
+                    if (publication instanceof MavenPublication mavenPublication) {
+                        mavenPublication.pom(mavenPom -> mavenPom.withXml(xmlProvider -> {
+                            Node depsNode = new Node(null, "dependencies");
+
+                            // Jank solution to an annoying issue
+                            project.getConfigurations().getByName("transitiveImplementation").getDependencies().forEach(dependency -> {
+                                Node depNode = depsNode.appendNode("dependency");
+                                depNode.appendNode("groupId", dependency.getGroup());
+                                depNode.appendNode("artifactId", dependency.getName());
+                                depNode.appendNode("version", dependency.getVersion());
+                                depNode.appendNode("scope", "runtime");
+                            });
+
+                            // Replace the dependency block, because it's just hopelessly wrong and includes floader+asm for some reason
+                            ((Node) ((NodeList) xmlProvider.asNode().get("dependencies")).get(0)).replaceNode(depsNode);
+                        }));
                     }
                 });
             }
